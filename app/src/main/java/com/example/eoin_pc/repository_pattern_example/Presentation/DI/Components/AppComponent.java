e6614cc0a4931d25576daa315d4875f90265090f
eoinahern <eoin.ahern@mycit.ie>
2016-08-31 20:12:14 +0100

    added presentation layer

---
+++ app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/DI/Components/AppComponent.java
@@ -0,0 +1,7 @@
+package com.example.eoin_pc.repository_pattern_example.Presentation.DI.Components;
+
+/**
+ * Created by eoin_a on 31/08/2016.
+ */
+public interface AppComponent {
+}
---
+++ app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/DI/Modules/AppModule.java
@@ -0,0 +1,7 @@
+package com.example.eoin_pc.repository_pattern_example.Presentation.DI.Modules;
+
+/**
+ * Created by eoin_a on 31/08/2016.
+ */
+public class AppModule {
+}
