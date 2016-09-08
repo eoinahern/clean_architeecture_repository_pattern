65bc0100cda5665f75ae240520266a5deb0468ff
eoinahern <eoin.ahern@mycit.ie>
2016-09-08 19:26:49 +0100

    add weather presenter

--- app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/Presenters/WeatherListPresenter.java
+++ app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/Presenters/WeatherListPresenter.java
@@ -4,4 +4,10 @@ package com.example.eoin_pc.repository_pattern_example.Presentation.Presenters;
  * Created by eoin_a on 06/09/2016.
  */
 public class WeatherListPresenter implements Presenter {
+
+
+    // in the presenter we need the use case tp carry out
+    // and the subscriber to get the data
+    // also the view interface.
+    //we the inject presenter into our view/fragment
 }
