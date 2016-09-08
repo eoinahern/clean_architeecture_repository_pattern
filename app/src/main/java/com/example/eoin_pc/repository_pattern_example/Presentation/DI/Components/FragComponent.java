3f9408ff15b7258e5651bcc46a6ef7ca12ff3247
eoinahern <eoin.ahern@mycit.ie>
2016-09-08 21:43:16 +0100

    added moduleand subcomponent for fragment

---
+++ app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/DI/Components/FragComponent.java
@@ -0,0 +1,7 @@
+package com.example.eoin_pc.repository_pattern_example.Presentation.DI.Components;
+
+/**
+ * Created by eoin_a on 08/09/2016.
+ */
+public interface FragComponent {
+}
---
+++ app/src/main/java/com/example/eoin_pc/repository_pattern_example/Presentation/DI/Modules/FragModule.java
@@ -0,0 +1,7 @@
+package com.example.eoin_pc.repository_pattern_example.Presentation.DI.Modules;
+
+/**
+ * Created by eoin_a on 08/09/2016.
+ */
+public class FragModule {
+}
